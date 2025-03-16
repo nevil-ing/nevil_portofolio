98dc6dfab0e22bc04e9a47c7bb951e2a0cdd8eda
nevilpurpp <nevilpurpp12@gmail.com>
2025-03-16 23:46:22 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-        builder: (context, child) => ResponsiveBreakpoints.builder(
+        builder: (context) => ResponsiveBreakpoints.builder(
           breakpoints: [
             const Breakpoint(start: 0, end: 450, name: MOBILE),
             const Breakpoint(start: 451, end: 800, name: TABLET),
             const Breakpoint(start: 801, end: 1920, name: DESKTOP),
             const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
           ],
-          child: child!,
         ),
       home: const MyHomePage()
     );
